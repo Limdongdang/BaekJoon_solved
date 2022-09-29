4dfc3b0ec2220bbfdbba13864f3d6bb660c60f79
림동댕 <50188317+Limdongdang@users.noreply.github.com>
2022-09-29 21:17:13 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    for(int j = 0; j < n; j++){
+        for(int i = 0; i < n - j - 1; i++){
+            cout << " ";
+        }
+        for(int i = 0; i < 2 * j + 1; i++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for(int j = 1; j < n; j++){
+        for(int i = 0; i < j; i++){
+            cout << " ";
+        }
+        for(int i = 0; i < (2 * n - 2 * j) - 1;i++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
