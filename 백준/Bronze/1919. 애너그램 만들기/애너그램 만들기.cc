f3dc8eb043e1b01b6019bb87b8e32b74d47022c9
림동댕 <50188317+Limdongdang@users.noreply.github.com>
2022-10-09 17:20:57 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.cc
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.cc
@@ -8,38 +8,19 @@ int main(void) {
 	
 	string fi, se;
 	cin >> fi >> se;
-	int p1 = 0, p2 = 0, cnt = 0;
-	sort(fi.begin(), fi.end());
-	sort(se.begin(), se.end());
-	while (p1 != fi.length() || p2 != se.length()) {
-		if (p1 == fi.length())
-		{
-			p2++;
-			cnt++;
-			continue;
-		}
-		else if (p2 == se.length()) {
-			p1++;
-			cnt++;
-			continue;
-		}
-		else {
-			if (fi[p1] != se[p2]) {
-				if (fi[p1] > se[p2]) {
-					p2++;
-					cnt++;
-				}
-				else {
-					p1++;
-					cnt++;
-				}
-			}
-			else {
-				p1++;
-				p2++;
-			}
-		}
-		
+	int cnt = 0;
+	int ls[2][26] = { 0 };
+	for (int i = 0; i < fi.length(); i++)
+	{
+		ls[0][fi[i] - 'a']++;
+	}
+	for (int i = 0; i < se.length(); i++)
+	{
+		ls[1][se[i] - 'a']++;
+	}
+	for (int i = 0; i < 26; i++)
+	{
+		cnt += abs(ls[0][i] - ls[1][i]);
 	}
 	cout << cnt;
 	return 0;
