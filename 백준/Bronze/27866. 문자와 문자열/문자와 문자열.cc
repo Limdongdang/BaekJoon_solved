a44d1cbc9d0b219b255afc33d037e96c9ec928e0
LimDong <50188317+Limdongdang@users.noreply.github.com>
2023-06-14 00:04:50 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string S;
+    short i;
+    cin >> S;
+    cin >> i;
+
+    cout << S[i-1];
+
+    return 0;
+}
+
+
+ 
